772a49a30142b33a2dbb4d865babc107ec6d5744
virajvchavan <viraj@joshsoftware.com>
2018-10-19 18:46:11 +0530

    submit inputs on enter key press

--- src/components/ChatForm.jsx
+++ src/components/ChatForm.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
 class ChatForm extends React.Component {
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      document.getElementById('send_btn').click();
+    }
+  }
+
   render() {
     return (
       <div id='chat_form'>
         <div className='form-inline' >
-          <input type='text' className='form-control col-md-11' id='message_content' />
-          <button className='btn btn-success' onClick={this.props.send_message}>Send</button>
+          <input type='text' className='form-control col-md-11' id='message_content' onKeyPress={this.handleKeyPress} />
+          <button className='btn btn-success' id='send_btn' onClick={this.props.send_message}>Send</button>
         </div>
       </div>
     )
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 
 class LoginForm extends React.Component {
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      document.getElementById('login_btn').click();
+    }
+  }
+
   render() {
     return (
       <div id='login_form' className='form-inline'>
         <div className='container'>
           <div className='jumbotron'>
             <h3>Enter your username:</h3>
-            <input type='text' className='form-control col-md-11' id='username_input' />
-            <button className='btn btn-success' onClick={this.props.login}>Start Chatting</button>
+            <input type='text' className='form-control col-md-11' id='username_input' onKeyPress={this.handleKeyPress} />
+            <button className='btn btn-success' id='login_btn' onClick={this.props.login}>Start Chatting</button>
           </div>
         </div>
       </div>
