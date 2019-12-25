a22e5a164b300707ce0f88a8f030a645872db26c
Viraj Chavan <virajc@live.com>
2019-12-25 22:17:26 +0530

    updates

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -16,6 +16,9 @@ class LoginForm extends React.Component {
             <h3>Enter your username:</h3>
             <input type='text' className='form-control col-md-11' id='username_input' onKeyPress={this.handleKeyPress} />
             <button className='btn btn-success' id='login_btn' onClick={this.props.login}>Start Chatting</button>
+            <br/> <br/>
+            <div>Note: This app sucks! I had made it to <strong>learn</strong> ReactJs only. The backend is written in a few minutes to support the UI somehow. So there's no login, nothing!
+            <br/><br/>Whatever username you enter above, the user will be logged in if the username exists. If not, a new user with that username is created! 😂</div>
           </div>
         </div>
       </div>
